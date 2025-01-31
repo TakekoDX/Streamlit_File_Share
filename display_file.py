cb488ce08a21f4983a211eb9e00f27394fc0092d
Yurika <physmath8lt@gmail.com>
2025-01-31 21:56:29 +0900

    add us screened file

--- display_file.py
+++ display_file.py
@@ -6,6 +6,7 @@ import glob
 # ディレクトリとファイルの形式
 RESULT_DIR = "data"
 FILE_PATTERN = "screened_stocks_*.csv"  # ワイルドカードでマッチするファイル形式
+FILE_PATTERN_US = "screened_stocks_us_*.csv"  # ワイルドカードでマッチするファイル形式
 
 st.title("スクリーニング結果")
 
@@ -19,19 +20,23 @@ def get_latest_file(directory, pattern):
 
 # 最新のスクリーニング結果ファイルを取得
 latest_file = get_latest_file(RESULT_DIR, FILE_PATTERN)
+latest_file_us = get_latest_file(RESULT_DIR, FILE_PATTERN_US)
 
 if latest_file:
-    st.subheader("最新のスクリーニング結果")
+    st.subheader("日本株のスクリーニング結果")
     df = pd.read_csv(latest_file)
 
     # Symbolカラムを文字列型に変換
     if 'Symbol' in df.columns:
         df['Symbol'] = df['Symbol'].astype(str)
     
-    # インデックスをリセットして表示
-    df = df.reset_index(drop=True)
-
     st.dataframe(df)
     st.success(f"表示中のファイル: {os.path.basename(latest_file)}")
 else:
     st.warning("スクリーニング結果がまだ生成されていません。")
+
+if latest_file_us:
+    st.subheader("米国株のスクリーニング結果")
+    df = pd.read_csv(latest_file_us)
+    st.dataframe(df)
+    st.success(f"表示中のファイル: {os.path.basename(latest_file_us)}")
